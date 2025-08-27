5fb6f801d3fc6f283644e57b1bce9afb1a15a22b
magniventures <magniventures@proton.me>
2025-08-27 22:14:11 +0100

    api-gateway, error-logging and setup

--- apps/auth-service/src/main.ts
+++ apps/auth-service/src/main.ts
@@ -1,14 +1,32 @@
 import express from 'express';
+import cors from "cors";
+import { errorMiddleware } from '../../../packages/error-handler/error-middleware';
 
-const host = process.env.HOST ?? 'localhost';
-const port = process.env.PORT ? Number(process.env.PORT) : 3000;
+
+//const host = process.env.HOST ?? '0.0.0.0';
+//const port = process.env.PORT ? Number(process.env.PORT) : 6001;
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["http://localhost:3000"],
+    allowedHeaders: ["Authorization", "Content-Type"],
+    credentials: true
+  })
+);
 
 app.get('/', (req, res) => {
     res.send({ 'message': 'Hello API'});
 });
 
-app.listen(port, host, () => {
-    console.log(`[ ready ] http://${host}:${port}`);
+app.use(errorMiddleware);
+
+const port = process.env.PORT || 6001;
+const server = app.listen(port, () => {
+  console.log(`Auth Service is Running At http://localhost:${port}/api`);
+});
+
+server.on("error", (err) => {
+  console.log("Server Error:", err);
 });
---
+++ packages/error-handler/index.ts
@@ -0,0 +1,56 @@
+export class AppError extends Error {
+  public readonly statusCode: number;
+  public readonly isOperational: boolean;
+  public readonly details: any;
+
+  constructor(message: string, statusCode: number, isOperational = true, details? : any) {
+    super(message);
+    this.statusCode = statusCode;
+    this.isOperational = isOperational;
+    this.details = details;
+    Error.captureStackTrace(this, AppError);
+  }
+}
+
+//NOT FOUND ERROR
+export class NotFoundError extends AppError {
+  constructor(message = "Resources not found", statusCode: number,) {
+    super(message, 404);
+  }
+}
+
+//VALIDATION ERROR (used for joi/zod/react-hook-form validation errors)
+
+export class validationError extends AppError {
+  constructor(message = "Invalid data", details? : any) {
+    super (message, 400, true, details);
+  }
+}
+
+//Authentication Error
+export class AuthError extends AppError {
+  constructor(message = "Unauthorized", details? : any) {
+    super (message, 401);
+  }
+}
+
+//Forbidden Error (For In sufficient Permissions)
+export class ForbiddenError extends AppError {
+  constructor(message = "Access Forbidden", details? : any) {
+    super (message, 403);
+  }
+}
+
+//Database Error (For MongoDB/Postgres Errors)
+export class DatabaseError extends AppError {
+  constructor(message = "Database Error", details? : any) {
+    super (message, 500, true, details);
+  }
+}
+
+//RATE LIMIT ERROR (If user exceeds API limits)
+export class RateLimitError extends AppError {
+  constructor(message = "Too many requests, please tryagain later", details? : any) {
+    super (message, 429);
+  }
+}
