6cc7ff08546a7ca84cb0fa33d38b457b8320ea8c
magniventures <magniventures@proton.me>
2025-09-03 19:10:41 +0100

    webpack.config.js

--- apps/auth-service/webpack.config.js
+++ apps/auth-service/webpack.config.js
@@ -3,23 +3,22 @@ const { join } = require('path');
 
 module.exports = {
   output: {
-    path: join(__dirname, 'dist'),
+    path: join(__dirname, '../../dist/apps/auth-service'),
     ...(process.env.NODE_ENV !== 'production' && {
       devtoolModuleFilenameTemplate: '[absolute-resource-path]',
     }),
-    
   },
   plugins: [
     new NxAppWebpackPlugin({
       target: 'node',
       compiler: 'tsc',
-      main: './src/main.ts',
-      tsConfig: './tsconfig.app.json',
-      assets: ["./src/assets"],
+      main: join(__dirname, 'src/main.ts'),
+      tsConfig: join(__dirname, 'tsconfig.app.json'),
+      assets: [join(__dirname, 'src/assets')],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
       sourceMaps: true,
-    })
+    }),
   ],
 };
