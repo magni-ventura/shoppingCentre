6910a0b2b1394c18a0738f409f85b285a2412493
magniventures <magniventures@proton.me>
2025-09-10 20:54:15 +0100

    Chore: auth-service nx setup issues mahn

--- apps/auth-service/webpack.config.js
+++ apps/auth-service/webpack.config.js
@@ -2,7 +2,8 @@ const { NxAppWebpackPlugin } = require('@nx/webpack/app-plugin');
 const { join, resolve } = require('path');
 
 module.exports = {
-  mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
+  mode: process.env.NODE_ENV === "production" ? "production" : "development",
+  entry: join(__dirname, 'src/main.ts'),
   output: {
     path: join(__dirname, '../../dist/apps/auth-service'),
     ...(process.env.NODE_ENV !== 'production' && {
@@ -21,10 +22,7 @@ module.exports = {
       compiler: 'tsc',
       main: join(__dirname, 'src/main.ts'),
       tsConfig: join(__dirname, 'tsconfig.app.json'),
-      assets: [
-        join(__dirname, 'src/assets'),
-        join(__dirname, 'swaggeroutput.json'),
-      ],
+      assets: [join(__dirname, 'src/swagger.json')],
       optimization: false,
       outputHashing: 'none',
       generatePackageJson: true,
